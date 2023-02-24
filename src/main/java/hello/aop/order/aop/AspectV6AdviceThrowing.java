67de08a093ee91ea756155cf39cdf1a72a5c9ac6
이상헌 <ddmczp@naver.com>
2023-02-24 13:04:54 +0900

    @Around로 AOP에서 에러 캐치하는 방법 구현
    - AOP에서 에러 캐치하는 방법 구현 후 주석 처리

--- src/main/java/hello/aop/order/aop/AspectV6AdviceThrowing.java
+++ src/main/java/hello/aop/order/aop/AspectV6AdviceThrowing.java
@@ -2,8 +2,10 @@ package hello.aop.order.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 @Slf4j
@@ -15,9 +17,22 @@ public class AspectV6AdviceThrowing {
         log.info("[doThrowingSerivce] {} message={}", joinPoint.getSignature(),
                 ex.getMessage());
     }
-//    @AfterThrowing(value = "execution(* hello.aop.order..*Repository.*(..))", throwing = "ex")
-//    public void doThrowingRepository(JoinPoint joinPoint, Exception ex) {
-//        log.info("[doThrowingRepository] {} message={}", joinPoint.getSignature(),
-//                ex.getMessage());
+    @AfterThrowing(value = "execution(* hello.aop.order..*Repository.*(..))", throwing = "ex")
+    public void doThrowingRepository(JoinPoint joinPoint, Exception ex) {
+        log.info("[doThrowingRepository] {} message={}", joinPoint.getSignature(),
+                ex.getMessage());
+    }
+
+//    @Around(value = "execution(* hello.aop.order..*Repository.*(..))")
+//    public void doAround(ProceedingJoinPoint joinPoint) {
+//        try {
+//            joinPoint.proceed();
+//        } catch (Exception ex) {
+//            log.info("[Exception] {} message={}", joinPoint.getSignature(),
+//                    ex.getMessage());
+//        } catch (Throwable ex) {
+//            log.info("[Throwable] {} message={}", joinPoint.getSignature(),
+//                    ex.getMessage());
+//        }
 //    }
 }
